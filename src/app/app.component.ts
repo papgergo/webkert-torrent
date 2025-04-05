4f6055b88b9e7597c99802bed159fd4788ee2c13
Pap Gergő Rajmund <h268307@stud.u-szeged.hu>
2025-04-05 18:46:07 +0200

    Basic styling and menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MenuComponent } from './shared/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, HomeComponent],
+  imports: [
+    RouterOutlet,
+    MenuComponent,
+    HomeComponent,
+    MatSidenavModule,
+    RouterLink,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'torrent-webpage';
+
+  ngOnInit(): void {}
+
+  onToggleSidenav(sidenav: MatSidenav) {
+    sidenav.toggle();
+  }
 }
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [RouterLink, MatListModule, MatIconModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  @Input() sidenav!: MatSidenav;
 
+  ngOnInit(): void {
+    console.log('szia');
+  }
+
+  closeMenu() {
+    this.sidenav.toggle();
+  }
 }
