3ec34dbcd3c49e485de890216398275e5f5af5ab
Pap Gergő Rajmund <h268307@stud.u-szeged.hu>
2025-04-05 18:46:42 +0200

    Torrents page creation

--- src/app/pages/torrents/torrents.component.ts
+++ src/app/pages/torrents/torrents.component.ts
@@ -1,11 +1,99 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { Torrent } from '../../shared/models/torrent';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { CategoryEnum } from '../../shared/models/category';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-torrents',
-  imports: [],
+  imports: [MatListModule, MatPaginatorModule, MatTableModule, MatIconModule],
   templateUrl: './torrents.component.html',
-  styleUrl: './torrents.component.scss'
+  styleUrl: './torrents.component.scss',
 })
-export class TorrentsComponent {
+export class TorrentsComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  displayedColumns: string[] = [
+    'category', //Category
+    'name', //Name
+    'seeds', //Seeds
+    'size', //Size
+    'uploadDate', //Upload date
+    'uploader', //Uploader
+  ];
+  torrentCollection: Torrent[] = [
+    {
+      id: 0,
+      category: { id: 0, name: 'MOVIE' },
+      name: 'Kedvenc film',
+      seeds: 20,
+      size: '20 GB',
+      createdDate: new Date(),
+      uploader: { id: 0, name: 'Pista', profilePictureUrl: '' },
+      description: 'asd',
+      coverImageUrl: 'asd',
+    },
+    {
+      id: 1,
+      category: { id: 0, name: 'GAME' },
+      name: 'Kedvenc film',
+      seeds: 20,
+      size: '20 GB',
+      createdDate: new Date(),
+      uploader: { id: 0, name: 'Pista', profilePictureUrl: '' },
+      description: 'asd',
+      coverImageUrl: 'asd',
+    },
+    {
+      id: 2,
+      category: { id: 0, name: 'SONG' },
+      name: 'Kedvenc film',
+      seeds: 20,
+      size: '20 GB',
+      createdDate: new Date(),
+      uploader: { id: 0, name: 'Pista', profilePictureUrl: '' },
+      description: 'asd',
+      coverImageUrl: 'asd',
+    },
+    {
+      id: 3,
+      category: { id: 0, name: 'PROGRAM' },
+      name: 'Kedvenc film',
+      seeds: 20,
+      size: '20 GB',
+      createdDate: new Date(),
+      uploader: { id: 0, name: 'Pista', profilePictureUrl: '' },
+      description: 'asd',
+      coverImageUrl: 'asd',
+    },
+    {
+      id: 3,
+      category: { id: 0, name: 'BOOK' },
+      name: 'Kedvenc film',
+      seeds: 20,
+      size: '20 GB',
+      createdDate: new Date(),
+      uploader: { id: 0, name: 'Pista', profilePictureUrl: '' },
+      description: 'asd',
+      coverImageUrl: 'asd',
+    },
+  ];
+
+  dataSource = new MatTableDataSource<Torrent>(this.torrentCollection);
+
+  getCategoryIcon(categoryName: string): string {
+    return CategoryEnum[categoryName as keyof typeof CategoryEnum];
+  }
 }
--- src/app/shared/models/category.ts
+++ src/app/shared/models/category.ts
@@ -1,4 +1,12 @@
+export enum CategoryEnum {
+  MOVIE = 'movie',
+  GAME = 'sports_esports',
+  SONG = 'music_note',
+  PROGRAM = 'computer',
+  BOOK = 'menu_book',
+}
+
 export interface Category {
-    id: number;
-    name: string;
+  id: number;
+  name: keyof typeof CategoryEnum;
 }
